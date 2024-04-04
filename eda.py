b977e94e1037f2ba02d4ab2894264036f85f4bb5
Lokesh Manideep <lokeshmanideep14@gmmail.com>
2024-04-04 18:36:46 -0500

    added descriptions and visualization

--- eda.py
+++ eda.py
@@ -13,3 +13,18 @@ def filter_columns(df,cols):
 def filter_rows(df):
     df = df[(df['CATEGORY']=='species') & (df['PROTOCOL TYPE']=='Traveling') | (df['PROTOCOL TYPE']=='Stationary') & (df['ALL SPECIES REPORTED']==1)]
     return df
+
+### Shannon index by year and month
+def shannon_index(species_abundance):
+    total_count = sum(species_abundance)
+    proportions = [count / total_count for count in species_abundance]
+    shannon_index = -sum(p * np.log(p) for p in proportions if p != 0)
+    return shannon_index
+
+def distribution(df,col):
+    df_new = df.groupby(col)[col].size().reset_index(name='Size')
+    df_new['Percent'] = (df_new['Size'] / df_new['Size'].sum()) * 100
+    df_new = df_new.sort_values(by="Percent",ascending=False)
+    print("\033[1m{} distribution\033[0m\n".format(col))
+    print(df_new)
+    print("\n\n")
