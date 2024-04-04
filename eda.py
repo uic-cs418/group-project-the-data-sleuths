abeb75099dee86ea4162e81b07d506264f2bda99
Lokesh Manideep <lokeshmanideep14@gmmail.com>
2024-04-04 17:03:48 -0500

    new notebook without chicago communities

--- eda.py
+++ eda.py
@@ -13,3 +13,10 @@ def filter_columns(df,cols):
 def filter_rows(df):
     df = df[(df['CATEGORY']=='species') & (df['PROTOCOL TYPE']=='Traveling') | (df['PROTOCOL TYPE']=='Stationary') & (df['ALL SPECIES REPORTED']==1)]
     return df
+
+### Shannon index by year and month
+def shannon_index(species_abundance):
+    total_count = sum(species_abundance)
+    proportions = [count / total_count for count in species_abundance]
+    shannon_index = -sum(p * np.log(p) for p in proportions if p != 0)
+    return shannon_index
